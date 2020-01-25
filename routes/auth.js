14ff0e62fc8ee255a59dddf1b42c208f21455f3a
Analin Flores <foglzerika@gmail.com>
2020-01-25 13:23:29 -0600

    fix resolve errors4

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 
+const User = require('./../models/User');
 /* GET signUp */
 router.get('/', (req, res) => {
   console.log("auth")
@@ -13,13 +14,15 @@ router.get('/registerUser', (req, res) => {
 });
 
 router.post('/registerUser' , (req,res) =>{
-  console.log(req.body);
- //res.render("");
+    const {name, email, password, phone} = req.body;
+    const newUser = new User({name, email, password, phone});
+   console.log(newUser)
+    newUser.save()
+    .then(user =>{
+      console.log("Register new user sucessfully");
+
+      res.redirect('/');
+    })
+    .catch(err => console.log("Something goes bad"));
 })
 module.exports = router;
-/* app.post('/petsInfo' , (req, res) =>{
-  console.log(req.body);
-  registerPet(req.body);
-   res.render("index")
-   
-}) */
